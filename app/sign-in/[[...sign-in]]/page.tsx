2d2803c367abad176e5d21826ca092ffb5747d5f
William Buchwalter <wbuchwalter@gmail.com>
2024-04-01 16:36:49 -0700

    update redirectUrl

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center py-24">
-      <SignIn redirectUrl="/dashboard" />
+      <SignIn redirectUrl="/dashboard2" />
     </div>
   );
 }
