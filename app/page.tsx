d2bbe54ebf8a62c96622dd3268e000ddea93893e
William Buchwalter <wbuchwalter@gmail.com>
2024-04-01 19:50:06 -0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           </p>
           <div className="flex gap-2 mt-8">
             <Link
-              href="https://testpaca.com/dashboard"
+              href="https://testpaca.com/dashboard33"
               className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700"
             >
               View Demo
