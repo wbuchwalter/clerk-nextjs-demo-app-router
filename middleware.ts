caba3e62d8ed766356aaf0e3a8d41f393e5fc81e
John Roberts <john@alpacaml.com>
2024-04-02 18:52:12 -0400

    add redirect to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,30 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // @ts-ignore
 export default authMiddleware({
   publicRoutes: ["/"],
-  debug: true,
-  domain: "www.testpaca.com",
+  // debug: true,
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //  For public routes, we don't need to do anything
+      return NextResponse.next();
+    }
+
+    const url = new URL(req.nextUrl.origin);
+
+    if (!auth.userId) {
+      //  If user tries to access a private route without being authenticated,
+      //  redirect them to the sign in page
+      const initialUrl = new URL(req.url);
+      console.log(req.headers);
+      url.pathname = `/sign-in`;
+      if (initialUrl.pathname) {
+        url.search = `?redirect_url=${initialUrl.pathname}`;
+      }
+      return NextResponse.redirect(url);
+    }
+  },
 });
 
 export const config = {
