f5fd8f00da9b3881cd5b3e3169f2a841e7673968
William Buchwalter <wbuchwalter@gmail.com>
2024-04-01 17:05:52 -0700

    update redirectUrl

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           </p>
           <div className="flex gap-2 mt-8">
             <Link
-              href="/dashboard"
+              href="https://testpaca.com/dashboard"
               className="flex content-center gap-2 px-4 py-2 font-semibold text-white transition-colors duration-200 rounded-lg bg-primary-600 hover:bg-primary-700"
             >
               View Demo
