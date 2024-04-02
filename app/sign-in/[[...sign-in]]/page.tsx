c546f7c07b57f5731c8208b2be34fd573f0a20b9
John Roberts <john@alpacaml.com>
2024-04-02 19:10:05 -0400

    copy over redirectURL handling from alpaca app

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
+  const params = useSearchParams();
+  const redirectURL = params.get("redirect_url") || "/dashboard";
   return (
     <div className="flex justify-center py-24">
-      <SignIn afterSignInUrl="https://testpaca.com/dashboard2" />
+      <SignIn signUpUrl="/sign-up" redirectUrl={redirectURL} />
     </div>
   );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,12 @@
 import { SignUp } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
+  const params = useSearchParams();
+  const redirectURL = params.get("redirect_url") || "/dashboard";
   return (
     <div className="flex justify-center py-24">
-      <SignUp />
+      <SignUp signInUrl="/sign-in" redirectUrl={redirectURL} />
     </div>
   );
 }
